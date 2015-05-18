a6548d8082c5e3cc457a693312fd2b2680bdf64e
Art Dogan <artdog@amazon.com>
2015-05-17 17:35:05 -0700

    Renamed ViewController to ConsentViewController
    Change-Id: Ib4f26ae10478a9e7efabeb29f1ed430e3524f12a

--- Xcoders ResearchKit Demo3/ConsentViewController.swift
+++ Xcoders ResearchKit Demo3/ConsentViewController.swift
@@ -1,35 +1,63 @@
 //
-//  ConsentViewController.swift
+//  ViewController.swift
 //  Xcoders ResearchKit Demo3
 //
-//  Created by Dogan, Art on 5/13/15.
+//  Created by Dogan, Art on 5/12/15.
 //  Copyright (c) 2015 artdog, Inc. All rights reserved.
 //
 
 import UIKit
+import ResearchKit
 
-class ConsentViewController: UIViewController {
+class ConsentViewController: UIViewController, ORKTaskViewControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func launchConsent(sender: AnyObject) {
+        let consentSteps = IFConsent().studyOverviewSteps()
+        let task = ORKOrderedTask(identifier: "identifier", steps: [consentSteps, IFConsent().reviewConsent()])
+        
+        let taskViewController = ORKTaskViewController(task: task, taskRunUUID: nil)
+        taskViewController.delegate = self
+        
+        self.presentViewController(taskViewController, animated: true, completion: nil)
     }
-    */
-
+    
+    
+    @IBAction func reviewAndAgreeConsent(sender:AnyObject) {
+        let task = ORKOrderedTask(identifier: "reviewConsent", steps: [IFConsent().reviewConsent()])
+        let taskViewController = ORKTaskViewController(task: task, taskRunUUID: nil)
+        taskViewController.delegate = self
+        
+        self.presentViewController(taskViewController, animated: true, completion: nil)
+    }
+    
+    
+    // MARK: ORKTaskViewControllerDelegate
+    
+    func taskViewController(taskViewController: ORKTaskViewController, didFinishWithReason reason: ORKTaskViewControllerFinishReason, error: NSError?) {
+        /*
+        The `reason` passed to this method indicates why the task view
+        controller finished: Did the user cancel, save, or actually complete
+        the task; or was there an error?
+        
+        The actual result of the task is on the `result` property of the task
+        view controller.
+        */
+        //taskResultFinishedCompletionHandler?(taskViewController.result)
+        
+        taskViewController.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
 }
+
---
+++ Xcoders ResearchKit Demo3/MainViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MainViewController.swift
+//  Xcoders ResearchKit Demo3
+//
+//  Created by Dogan, Art on 5/17/15.
+//  Copyright (c) 2015 artdog, Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        
+    }
+    
+    
+    @IBAction func unwindToViewController (sender: UIStoryboardSegue){
+        
+    }
+}
